be1a3240f89c302105c61d5abcd41817fe40ce83
Sqkins <53656183+Sqkins@users.noreply.github.com>
2020-08-19 21:41:23 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ app.get('/', function(req, res) {
 });
 
 var con = mysql.createConnection({
-  host: "localhost",
+  host: "127.0.0.1",
   port: "3306",
   user: "root",
-  password: "Marcus11",
-  database: "budgeteer",
+  password: "",
+  database: "budgetme",
   insecureAuth : true
 });
 
