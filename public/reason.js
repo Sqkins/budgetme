3eacee53e2290528000f41030e54768db85a12cd
Sqkins <53656183+Sqkins@users.noreply.github.com>
2020-08-20 11:33:51 +0100

    Add files via upload

--- public/reason.js
+++ public/reason.js
@@ -13,6 +13,7 @@ function updateReasons(){
   var sel = document.getElementById('select-option');
   for(var option in reasons) {
     var opt = document.createElement('option');
+    console.log(option);
     opt.appendChild( document.createTextNode(option.reason) );
     opt.value = option.reason;
     sel.appendChild(opt);
