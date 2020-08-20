1485e00b1dd52deea21f292ed03fa9f54ff07fe6
Sqkins <53656183+Sqkins@users.noreply.github.com>
2020-08-20 11:30:53 +0100

    Add files via upload

--- public/reason.js
+++ public/reason.js
@@ -5,4 +5,16 @@ function addReason() {
   var input = document.getElementById('new-reason');
   console.log(input.value);
   socket.emit('add-reason',input.value);
+  updateReasons();
+}
+
+function updateReasons(){
+    // get reference to select element
+  var sel = document.getElementById('select-option');
+  for(var option in reasons) {
+    var opt = document.createElement('option');
+    opt.appendChild( document.createTextNode(option) );
+    opt.value = option;
+    sel.appendChild(opt);
+  }
 }
--- public/sockets.js
+++ public/sockets.js
@@ -3,4 +3,5 @@ var socket = io.connect('http://134.122.109.226');
 socket.on('reasons-data',function(data) {
   console.log(data);
   reasons = data;
+  updateReasons();
 });
