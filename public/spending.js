f4c269ae7e9beb3c8f1a5cafbabc4bbd25cd9fba
Sqkins <53656183+Sqkins@users.noreply.github.com>
2020-08-20 17:38:00 +0100

    Add files via upload

--- public/spending.js
+++ public/spending.js
@@ -4,6 +4,8 @@ function addTransaction() {
   var reason = document.getElementById('options').value;
   var date = document.getElementById('date').value;
   var amount = document.getElementById('amount').value;
-
+  console.log(reason);
+  console.log(date);
+  console.log(amount);
   socket.emit('new-transaction',reason,date,amount);
 }
