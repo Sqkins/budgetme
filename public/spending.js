209f695673b96f82f4f6de13d88ef193bddb98bb
BuildTools <unconfigured@null.spigotmc.org>
2020-08-24 16:12:22 +0100

    g

--- public/spending.js
+++ public/spending.js
@@ -27,7 +27,7 @@ function showTransactions() {
   week_totalspend.innerHTML = "Spent: £"+weektotal.toFixed(2); //set the week total spend
   for (var reason in weekreasons) { //loop through the reasons for this week
     var amount = weekreasons[reason]; //get amount for reason
-    week_byreason.innerHTML += "<p class=\"w3-panel w3-blue\">"+reason + ": £" + amount.toFixed(2)+"</p>"; //add html
+    week_byreason.innerHTML += "<div class=\"w3-panel w3-blue\" style=\"margin: 5px;\"><p style=\"padding:5px;\">"+reason + ": £" + amount.toFixed(2)+"</p></div>"; //add html
   }
   for (var x in weekdaylist) { //loop through days in a week
     var amount = 0; //default amount
@@ -35,7 +35,7 @@ function showTransactions() {
     if(weekdays.hasOwnProperty(day)) { //if money spent on 'day' set amount to the spend
       amount = weekdays[day];
     }
-    week_byday.innerHTML += "<p class=\"w3-panel w3-blue\">"+day + ": £" + amount.toFixed(2)+"</p>"; //add html
+    week_byday.innerHTML += "<div class=\"w3-panel w3-blue\" style=\"margin: 5px;\"><p style=\"padding:5px;\">"+day + ": £" + amount.toFixed(2)+"</p></div>"; //add html
   }
 }
 
