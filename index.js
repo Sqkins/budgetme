f63b3c8caf381e5f680c50a4f1c22fcdd4ca0cdb
BuildTools <unconfigured@null.spigotmc.org>
2020-09-03 22:07:25 +0100

    FE

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var express = require('express');
 var socket = require('socket.io');
 var mysql = require('mysql');
 var path = require('path');
-
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
 //Setup App
 var app = express();
 
