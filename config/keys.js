b7df4eb47508008e19938f0dba2d2d405653ceb9
BuildTools <unconfigured@null.spigotmc.org>
2020-09-03 21:54:24 +0100

    add accounts

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://marcusadmin:Marcus11@todo-ggwrn.mongodb.net/accounts?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
--- index.js
+++ index.js
@@ -9,6 +9,57 @@ var server = app.listen(80, function() {
   console.log('Listening on port 80');
 });
 
+// Passport Config
+require('./config/passport')(passport);
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true ,useUnifiedTopology: true}
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// Routes
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
+
+
 app.use(express.static('public'));
 app.use(express.static('public/main'));
 app.use(express.static('public/mobile'));
