77f47de732ea2b2540bd38147c330c06c1f33f1f
BuildTools <unconfigured@null.spigotmc.org>
2020-09-10 10:31:34 +0100

    e

--- index.js
+++ index.js
@@ -74,19 +74,6 @@ app.use(express.static('public'));
 app.use(express.static('public/main'));
 app.use(express.static('public/mobile'));
 app.use(express.static('icon'));
-/*app.get('/legacy', function(req, res) {
-  var ua = req.header('user-agent');
-  // Check the user-agent string to identyfy the device.
-  if (/mobile|iphone|ipod|android|blackberry|opera|mini|windows\sce|palm|smartphone|iemobile|ipad|android|android 3.0|xoom|sch-i800|playbook|tablet|kindle/i.test(ua)) {
-    res.redirect("http://134.122.109.226/mobile/");
-  } else {
-    res.sendFile(path.join(__dirname + '/public/main/main.html'));
-  }
-
-});
-app.get('/mobile', function(req, res) {
-  res.sendFile(path.join(__dirname + '/public/mobile/mobile.html'));
-}); */
 app.get('/colors', function (req, res) {
   res.sendFile(path.join(__dirname + '/public/colors.css'));
 });
